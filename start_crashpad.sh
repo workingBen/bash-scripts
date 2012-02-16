ae570b20571f59950ddce0a10439f8b7daf7711e
Ben Pellow <benjmain.pellow@gmail.com>
2012-02-16 12:51:53 -0800

    start crashpad script

---
+++ start_crashpad.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+workspace_path="/Users/bpellow/workspace"
+name="Ben"
+greeting="Hey $name! CrashPad is started!"
+
+function run_cmd {
+return=`/usr/bin/osascript << EOT
+  activate app "Terminal"
+  tell application "System Events"
+    tell process "Terminal"
+      keystroke "t" using {command down}
+    end tell
+  end tell
+
+  delay 1
+
+  tell app "Terminal"
+    do script "cd $workspace_path/$1; $2" in the last tab of window 1
+  end tell
+EOT`
+echo $return
+}
+
+mongod run --config /usr/local/Cellar/mongodb/2.0.2-x86_64/mongod.conf &
+run_cmd 'crashpad' 'rails s --debugger'
+run_cmd 'crashpad' 'rails c'
+run_cmd 'okcl_user_service' 'rackup -p9292'
+run_cmd 'omniscience' 'rackup -p9393'
+clear
+echo $greeting
+say $greeting
